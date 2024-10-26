8c6c82ff932d16de653d8b211389ea6924fe6023
Bob Maertz <1771054+bobmaertz@users.noreply.github.com>
2024-10-26 00:17:24 -0400

    Fixing up old tests

--- pkg/authorize/authorize.go
+++ pkg/authorize/authorize.go
@@ -4,9 +4,9 @@ import (
 	"crypto/rand"
 	"fmt"
 	"net/url"
+	"os"
 	"time"
 
-    "log/slog"
 	oauthErr "github.com/bobmaertz/railcar/pkg/error"
 	"github.com/bobmaertz/railcar/pkg/storage"
 )
@@ -32,14 +32,11 @@ func NewAuthorizer(s storage.Backend) (Authorizer, error) {
 	return Authorizer{backend: s, generateAuthCodeFunc: defaultAuthCodeGenerator}, nil
 }
 
-//Authorize validates the authorization_code request and 
+// Authorize validates the authorization_code request and
 func (a *Authorizer) Authorize(req Request) (string, *oauthErr.OAuthError) {
-
-	//Validate that the client exists
+	// Validate that the client exists
 	client, err := a.backend.GetClient(req.ClientId)
-    slog.Info("clientID", req.ClientId)
 	if err != nil {
-		slog.Warn("Authorize: client is not authorized","error", err)
 		return "", oauthErr.Errors["unauthorized_client"]
 	}
 
@@ -52,22 +49,21 @@ func (a *Authorizer) Authorize(req Request) (string, *oauthErr.OAuthError) {
 }
 
 func (a *Authorizer) processAuthCodeRequest(client storage.Client, req Request) (string, *oauthErr.OAuthError) {
-	//TODO: The spec stays this is optional but leave mandatory for now.
+	// TODO: The spec stays this is optional but leave mandatory for now.
 	if !contains(client.RedirectUris, req.RedirectUri) {
 		return "", oauthErr.Errors["invalid_request"]
 	}
 
-	//TODO: The only reason we have to reference Authorizer is for this function and the logging,
+	// TODO: The only reason we have to reference Authorizer is for this function and the logging,
 	// can this be removed so this function can operate independantly?
 	code, err := a.generateAuthCodeFunc()
 	if err != nil {
-		slog.Error("processAuthCodeRequest: unable to create authorization code: %v", err)
+		fmt.Fprintf(os.Stderr, "processAuthCodeRequest: unable to create authorization code: %v", err)
 		return "", oauthErr.Errors["server_error"]
 	}
 
 	err = a.backend.CreateAuthorizationCode(code, client, time.Now().Add(10*time.Minute))
 	if err != nil {
-		slog.Error("processAuthCodeRequest: unable to save authorization code: %v", err)
 		return "", oauthErr.Errors["server_error"]
 	}
 
@@ -78,14 +74,13 @@ func (a *Authorizer) processAuthCodeRequest(client storage.Client, req Request) 
 
 	redirect, err := createRedirectUrl(req.RedirectUri, queries)
 	if err != nil {
-		slog.Error("processAuthCodeRequest: unable to create redirect url: %v", err)
 		return "", oauthErr.Errors["server_error"]
 	}
 
 	return redirect, nil
 }
 
-//defaultAuthCodeGenerator generates a random 16 byte string for the authorization code
+// defaultAuthCodeGenerator generates a random 16 byte string for the authorization code
 func defaultAuthCodeGenerator() (string, error) {
 	n := 16
 	b := make([]byte, n)
@@ -113,12 +108,11 @@ func createRedirectUrl(baseUrl string, queries map[string]string) (string, error
 	return u.String(), nil
 }
 
-
 func contains(arr []string, target string) bool {
-    for _, e := range arr {
-        if e == target {
-            return true
-        }
-    }
-    return false
+	for _, e := range arr {
+		if e == target {
+			return true
+		}
+	}
+	return false
 }
--- pkg/storage/memory/memory.go
+++ pkg/storage/memory/memory.go
@@ -2,62 +2,69 @@ package memory
 
 import (
 	"errors"
+	"sync"
 	"time"
 
-
-    "sync"
 	"github.com/bobmaertz/railcar/pkg/storage"
 )
 
-
 var (
-    NotImplementedError = errors.New("not implemented")
-    NotFoundError = errors.New("not found")
-) 
+	NotImplementedError = errors.New("not implemented")
+	NotFoundError       = errors.New("not found")
+)
+
 type Memory struct {
 	clients map[string]storage.Client
 	codes   []storage.AuthorizationCode
 	session []storage.Session
 
-    mu sync.Mutex
+	mu sync.Mutex
 }
 
+// NewMemory creates a new memory structure
 func NewMemory() (*Memory, error) {
-    return &Memory{clients: map[string]storage.Client{"29352735982374239857":{Name: "Mock Test Client", RedirectUris: []string{"http://localhost"}}}}, nil
+	return &Memory{
+		clients: map[string]storage.Client{
+			"29352735982374239857": {
+				Name:         "Mock Test Client",
+				RedirectUris: []string{"http://localhost"},
+			},
+		},
+	}, nil
 }
 
 func (m *Memory) GetClient(id string) (storage.Client, error) {
-    m.mu.Lock()
-    defer m.mu.Unlock()
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	for k, v := range m.clients {
 		if k == id {
-            v.Id = k
+			v.Id = k
 			return v, nil
 		}
 	}
-	return storage.Client{}, NotFoundError 
+	return storage.Client{}, NotFoundError
 }
 
 func (m *Memory) SaveClient(client storage.Client) error {
-    m.mu.Lock()
-    defer m.mu.Unlock()
+	m.mu.Lock()
+	defer m.mu.Unlock()
 
-    m.clients[client.Id] = client
+	m.clients[client.Id] = client
 
-    return nil
+	return nil
 }
 
 func (m *Memory) CreateAuthorizationCode(code string, client storage.Client, expiry time.Time) error {
-    m.mu.Lock()
-    defer m.mu.Unlock()
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	m.codes = append(m.codes, storage.AuthorizationCode{Code: code, ClientId: client.Id, Expiry: expiry})
 
 	return nil
 }
 
 func (m *Memory) CreateSession(userId string, scopes []string, client storage.Client, expiry time.Time) error {
-    m.mu.Lock()
-    defer m.mu.Unlock()
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	m.session = append(m.session, storage.Session{UserId: userId, ClientId: client.Id, Expiry: expiry, Scopes: scopes})
 
 	return nil
